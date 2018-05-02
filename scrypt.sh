3df8961d94f627418a9dce7e195322f3b7df5a3d
nicolas <lizwert.g@gmail.com>
2018-05-02 02:44:06 -0300

    arreglo de formato de fecha y hora

--- scrypt.sh
+++ scrypt.sh
@@ -1,6 +1,10 @@
-current_date_time="`date +%Y/%m/%d---%H:%M:%S`";
-echo $current_date_time
-./hola
 
+clear
 current_date_time="`date +%Y/%m/%d---%H:%M:%S`";
-echo $current_date_time
+echo Fecha y hora de comienzo: $current_date_time
+echo compilando programa...
+make
+echo ejecutando programa....
+./puzzle8
+current_date_time="`date +%Y/%m/%d---%H:%M:%S`";
+echo Fecha y hora de termino:  $current_date_time
